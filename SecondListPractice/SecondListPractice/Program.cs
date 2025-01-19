866313851f40d541d5023bc15a078fb2568a1012
yilmazcinar <cinaryilmaz96@gmail.com>
2025-01-19 19:38:00 +0300

    Add files via upload

---
+++ SecondListPractice/SecondListPractice/Program.cs
@@ -0,0 +1,16 @@
+﻿List<string> coffes = new List<string>();
+
+for (int i = 5; i > 0; i--)
+{
+    Console.WriteLine($"\n{i} adet kahve ismi giriniz.\n");
+    string coffee = Console.ReadLine().ToLower().Trim();
+    coffes.Add( coffee );
+
+}
+
+Console.WriteLine("\nGirmiş olduğunu kahve isimleri : \n");
+foreach (var item in coffes)
+
+{
+    Console.WriteLine($"\n{item}\n");
+}
